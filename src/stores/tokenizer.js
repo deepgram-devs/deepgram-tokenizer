abc4a32e92df8882d9a39f1cfdcf6afaeb05e42f
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-09-12 09:45:39 -0500

    update tokenization method

--- src/stores/tokenizer.js
+++ src/stores/tokenizer.js
@@ -1,9 +1,6 @@
 import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 import tokenizerJson from '../assets/models/bloomz-560m/tokenizer.json'
-import tokenizerConfig from '../assets/models/bloomz-560m/tokenizer_config.json'
-
-import CustomTokenizer from '../classes/CustomTokenizer'
 
 export const useTokenizerStore = defineStore('tokenizer', () => {
   const textInput = ref('')
@@ -21,13 +18,84 @@ export const useTokenizerStore = defineStore('tokenizer', () => {
     }
   )
 
-  async function tokenizeText() {
-    const tokenizer = new CustomTokenizer(tokenizerConfig, tokenizerJson)
-    subwordTokens.value = tokenizer.tokenize(textInput.value)
-    tokenIds.value = subwordTokens.value.map((subword) => {
-      const originalSubword = subword.trim()
-      return tokenizer.tokens_to_ids.get(originalSubword)
+  function pretokenize() {
+    const pattern = /[^\s.,!?…。，、।۔،]+|[.,!?…。，、।۔،]+/g
+    const subtokens = textInput.value.match(pattern) || []
+    return subtokens
+  }
+
+  function postprocessSubtokens(subtokens) {
+    let processedSubtokens = subtokens.map((subtoken, index) => {
+      if (index === 0) {
+        return subtoken
+      }
+      if (/^[.,!?…。，、।۔،]+$/.test(subtoken)) {
+        return subtoken
+      } else {
+        return 'Ġ' + subtoken
+      }
     })
+    return processedSubtokens
+  }
+
+  function replaceSymbolWithSpace(obj) {
+    for (const key in obj) {
+      if (
+        Object.prototype.hasOwnProperty.call(obj, key) &&
+        typeof obj[key] === 'string' &&
+        obj[key].startsWith('Ġ')
+      ) {
+        obj[key] = obj[key].replace('Ġ', ' ')
+      }
+    }
+    return obj
+  }
+
+  function tokenizeText() {
+    const vocab = tokenizerJson.model.vocab
+    const subtokens = pretokenize()
+    const processedSubtokens = postprocessSubtokens(subtokens)
+
+    const concatenatedText = processedSubtokens.join('')
+    const finalFormat = []
+    let startIndex = 0
+    let pushedItem = false
+
+    while (startIndex < concatenatedText.length) {
+      let endIndex = concatenatedText.length
+      let longestSubtoken = concatenatedText.slice(startIndex, endIndex)
+
+      while (endIndex >= startIndex) {
+        if (vocab[longestSubtoken] !== undefined && !longestSubtoken.endsWith('Ġ')) {
+          if (!pushedItem) {
+            finalFormat.push({ [vocab[longestSubtoken]]: longestSubtoken })
+            pushedItem = true // Set the flag
+          }
+          startIndex = endIndex
+          break
+        }
+        endIndex--
+        longestSubtoken = concatenatedText.slice(startIndex, endIndex)
+      }
+
+      if (pushedItem) {
+        pushedItem = false
+      }
+    }
+
+    const processedArray = finalFormat.map((obj) => replaceSymbolWithSpace(obj))
+
+    const valuesArray = []
+    const keysArray = []
+
+    processedArray.forEach((obj) => {
+      const values = Object.values(obj)
+      valuesArray.push(values.pop())
+      const keys = Object.keys(obj)
+      keysArray.push(keys.pop())
+    })
+    subwordTokens.value = valuesArray
+    tokenIds.value = keysArray
     filteredTokenIds.value = tokenIds.value.filter((value) => value !== undefined)
   }
 
