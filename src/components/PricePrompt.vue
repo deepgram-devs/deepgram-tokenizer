451da1bd23bdf4cc865fbdde8ca76e04a3ff04f2
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-09-15 12:57:22 -0500

    testing deployment

--- src/components/PricePrompt.vue
+++ src/components/PricePrompt.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="flex-grow rounded-[4px] border bg-raisinBlack p-4 shadow-sm border-[#2C2C33]">
     <p class="text-sm">Price</p>
-    <p class="text-lg"></p>
+    <!-- <p class="text-lg"></p> -->
   </div>
 </template>
